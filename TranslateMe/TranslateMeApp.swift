0532f2a2a953fa77d53939f4a7077183b6f5153d
Savantah <stherclair@gmail.com>
2024-10-30 18:58:18 -0400

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Star Lina on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslateMeTests/TranslateMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeTests.swift
+//  TranslateMeTests
+//
+//  Created by Star Lina on 10/30/24.
+//
+
+import Testing
+@testable import TranslateMe
+
+struct TranslateMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
